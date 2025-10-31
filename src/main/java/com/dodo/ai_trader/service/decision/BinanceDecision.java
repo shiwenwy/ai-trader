487d255bffd7620e97b3ceb6f64f928a8e64e12b
shiwen <wangyao99163@163com>
2025-10-31 10:06:15 +0800

    优化

--- src/main/java/com/dodo/ai_trader/service/decision/BinanceDecision.java
+++ src/main/java/com/dodo/ai_trader/service/decision/BinanceDecision.java
@@ -1,7 +1,10 @@
 package com.dodo.ai_trader.service.decision;
 
+import com.dodo.ai_trader.service.config.BizConfig;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,15 @@ import java.util.Map;
 @Component
 public class BinanceDecision {
 
+    @Autowired
     private ChatClient chatClient;
-
-    public BinanceDecision(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
-    }
+    @Autowired
+    private BizConfig bizConfig;
 
     @Value("classpath:/prompts/system-message.mustache")
     private Resource systemResource;
+
+    
 
     private static String jsonFormat = "{\n" +
             "  \"signal\": \"buy_to_enter\" | \"sell_to_enter\" | \"hold\" | \"close\",\n" +
@@ -34,19 +38,26 @@ public class BinanceDecision {
             "  \"justification\": \"<string>\"\n" +
             "}";
 
-    public String test(){
+    private Prompt buildSystemPrompt(Integer initialFunding) {
         Map<String, Object> variables = new HashMap<>();
-        variables.put("initialFunding", 10000);
-        variables.put("altcoinMin", 50);
-        variables.put("altcoinMax", 150);
-        variables.put("altcoinLeverage", 5);
-        variables.put("btcMin", 200);
-        variables.put("btcMax", 600);
-        variables.put("btcLeverage", 3);
+        variables.put("initialFunding", initialFunding);
+        variables.put("altcoinMin", bizConfig.getAltcoinMin());
+        variables.put("altcoinMax", bizConfig.getAltcoinMax());
+        variables.put("altcoinLeverage", bizConfig.getAltcoinLeverage());
+        variables.put("btcMin", bizConfig.getBtcMin());
+        variables.put("btcMax", bizConfig.getBtcMax());
+        variables.put("btcLeverage", bizConfig.getBtcLeverage());
         variables.put("jsonFormat", jsonFormat);
         SystemPromptTemplate template = new SystemPromptTemplate(systemResource);
-        String systemPrompt = template.render(variables);
-        return systemPrompt;
+
+        return template.create(variables);
+    }
+
+    public String test(){
+
+        Prompt systemPrompt = buildSystemPrompt(10000);
+
+        return systemPrompt.getContents();
     }
 
     public static void main(String[] args) {
